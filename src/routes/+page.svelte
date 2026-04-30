c1f8226c7d4995fdccf1dec79a78ab53b15833da
devyansh vishwakarma <devyanshnayak@gmail.com>
2026-04-30 15:21:49 +0530

    updated the website

---
+++ src/routes/+page.svelte
@@ -0,0 +1,26 @@
+<script>
+  import { DotLottieSvelte } from "@lottiefiles/dotlottie-svelte";
+</script>
+
+<div class="daddy">
+  <h1 class="hello">
+    hello
+    <span class="wavehand">
+      <DotLottieSvelte src="lottie.json" loop={false} autoplay />
+    </span>
+  </h1>
+</div>
+
+<style>
+  .hello {
+    display: flex;
+  }
+  .wavehand {
+    height: 50px;
+  }
+  .daddy {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+</style>
