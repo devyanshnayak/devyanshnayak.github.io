d7b22d8f9592f2c1f64f948c19c6842a44fa9595
devyansh vishwakarma <devyanshnayak@gmail.com>
2026-06-07 21:48:23 +0530

    open graph fix

--- src/lib/config.js
+++
@@ -1,7 +0,0 @@
-export const siteConfig = {
-  siteName: "Devyansh's Portfolio",
-  siteUrl: "https://devyansh.site",
-  description:
-    " portfolio of devyansh how is intrestted in Linux, DevOps, Kubernetes and Homelab projects",
-  author: "Devi",
-};
